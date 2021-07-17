4120e2daa3598059877e22b81c974bf085eb4606
Square789 <46634729+Square789@users.noreply.github.com>
2021-07-17 16:55:03 +0200

    Added license transfer to nuitka build for regex module

--- setup_nuitka.py
+++ setup_nuitka.py
@@ -1,9 +1,12 @@
 from datetime import datetime
+from itertools import product
 import os
 from pathlib import Path
 import re
 import shutil
 import subprocess
+
+import pkg_resources
 
 # files that the program still runs without (according
 # to ProcessExplorer) and that are relatively large in size.
@@ -37,6 +40,7 @@ PROBABLY_UNUSED_DIRS = (
 cur_path = Path.cwd()
 build_dir = cur_path / "nuitka_build"
 dist_dir = build_dir / "demomgr.dist"
+regex_dist_dir = dist_dir / "regex"
 run_py = cur_path / "demomgr"
 
 def main():
@@ -74,6 +78,22 @@ def main():
 		license = license_fp.read()
 	with (dist_dir / "LICENSE").open("w") as license_fp:
 		license_fp.write(re.sub(r"\(c\) \d+", datetime.now().strftime("(c) %Y"), license))
+
+	regex_license = None
+	regex_license_filename = None
+	regex_module = pkg_resources.get_distribution("regex")
+	for (name, ext) in product(("LICENSE", "LICENCE"), ("", ".txt")):
+		try:
+			regex_license_filename = name + ext
+			regex_license = regex_module.get_metadata(regex_license_filename)
+			break
+		except FileNotFoundError:
+			pass
+	else:
+		raise FileNotFoundError("could not find license for regex module")
+
+	with (regex_dist_dir / regex_license_filename).open("w") as license_fp:
+		license_fp.write(regex_license)
 
 if __name__ == "__main__":
 	main()
